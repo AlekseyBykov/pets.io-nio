3f96a2e8ca2ddae718bed770e48a1d51de83cd14
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-10-15 14:20:59 +0400

    Added test for writing all the chars of string to file using buffer.

--- src/test/java/alekseybykov/portfolio/io/chario/ReaderWriterTest.java
+++ src/test/java/alekseybykov/portfolio/io/chario/ReaderWriterTest.java
@@ -8,12 +8,10 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author  aleksey.n.bykov@gmail.com
@@ -28,9 +26,9 @@ class ReaderWriterTest extends IOTestBase {
     @SneakyThrows
     @DisplayName("Read all the chars from string one by one using buffer")
     void testReadAllCharsFromStringUsingBuffer() {
-        final String japaneseString = "いくつかのテキスト";
+        final String stringJapanese = "いくつかのテキスト";
         try (Reader reader = new BufferedReader(new InputStreamReader(
-                new ByteArrayInputStream(japaneseString.getBytes(charsetName))))) {
+                new ByteArrayInputStream(stringJapanese.getBytes(charsetName))))) {
 
             assertEquals('い', (char)reader.read());
             assertEquals('く', (char)reader.read());
@@ -48,12 +46,49 @@ class ReaderWriterTest extends IOTestBase {
     @SneakyThrows
     @DisplayName("Read all the chars from string one by one without buffer")
     void testReadAllCharsFromStringWithoutBuffer() {
-        final String chineseString = "人物";
+        final String stringChinese = "人物";
         try (Reader reader = new InputStreamReader(
-                new ByteArrayInputStream(chineseString.getBytes(charsetName)))) {
+                new ByteArrayInputStream(stringChinese.getBytes(charsetName)))) {
 
             assertEquals('人', (char)reader.read());
             assertEquals('物', (char)reader.read());
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Write all the chars of string to file using buffer")
+    void testWriteAllCharsFromStringToFile() {
+        final String stringHebrew = "שתי שורות";
+        try (Reader reader = new BufferedReader(new InputStreamReader(
+                new ByteArrayInputStream(stringHebrew.getBytes(charsetName))));
+             Writer writer = new BufferedWriter(new OutputStreamWriter(
+                     new FileOutputStream(destinationFile), charsetName))) {
+
+            assertTrue(destinationFile.exists());
+
+            int c;
+            while ((c = reader.read()) != -1) {
+                writer.write((char)c);
+            }
+
+            assertTrue(destinationFile.length() == 0);
+            writer.flush();
+            assertTrue(destinationFile.length() > 0);
+
+            Reader inputStreamReader = new InputStreamReader(new FileInputStream(destinationFile));
+
+            assertEquals('ש', inputStreamReader.read());
+            assertEquals('ת', inputStreamReader.read());
+            assertEquals('י', inputStreamReader.read());
+            assertEquals(' ', inputStreamReader.read());
+            assertEquals('ש', inputStreamReader.read());
+            assertEquals('ו', inputStreamReader.read());
+            assertEquals('ר', inputStreamReader.read());
+            assertEquals('ו', inputStreamReader.read());
+            assertEquals('ת', inputStreamReader.read());
+
+            inputStreamReader.close();
         }
     }
 }
