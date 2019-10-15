38586d6ca51e99e1280a23a2faadbe75b769b5f0
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-10-15 13:57:30 +0400

    Added test for reading all the chars from string one by one without buffer. Refactoring.

--- src/test/java/alekseybykov/portfolio/io/chario/ReaderWriterTest.java
+++ src/test/java/alekseybykov/portfolio/io/chario/ReaderWriterTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,24 +22,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Tests for some concepts of InputStreamReader and InputStreamWriter")
 class ReaderWriterTest extends IOTestBase {
 
+    private final String charsetName = "UTF-8";
+
     @Test
     @SneakyThrows
-    @DisplayName("Read all the chars from string one by one")
-    void test() {
+    @DisplayName("Read all the chars from string one by one using buffer")
+    void testReadAllCharsFromStringUsingBuffer() {
         final String japaneseString = "いくつかのテキスト";
-        final String charsetName = "UTF-8";
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+        try (Reader reader = new BufferedReader(new InputStreamReader(
                 new ByteArrayInputStream(japaneseString.getBytes(charsetName))))) {
 
-            assertEquals('い', (char)bufferedReader.read());
-            assertEquals('く', (char)bufferedReader.read());
-            assertEquals('つ', (char)bufferedReader.read());
-            assertEquals('か', (char)bufferedReader.read());
-            assertEquals('の', (char)bufferedReader.read());
-            assertEquals('テ', (char)bufferedReader.read());
-            assertEquals('キ', (char)bufferedReader.read());
-            assertEquals('ス', (char)bufferedReader.read());
-            assertEquals('ト', (char)bufferedReader.read());
+            assertEquals('い', (char)reader.read());
+            assertEquals('く', (char)reader.read());
+            assertEquals('つ', (char)reader.read());
+            assertEquals('か', (char)reader.read());
+            assertEquals('の', (char)reader.read());
+            assertEquals('テ', (char)reader.read());
+            assertEquals('キ', (char)reader.read());
+            assertEquals('ス', (char)reader.read());
+            assertEquals('ト', (char)reader.read());
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Read all the chars from string one by one without buffer")
+    void testReadAllCharsFromStringWithoutBuffer() {
+        final String chineseString = "人物";
+        try (Reader reader = new InputStreamReader(
+                new ByteArrayInputStream(chineseString.getBytes(charsetName)))) {
+
+            assertEquals('人', (char)reader.read());
+            assertEquals('物', (char)reader.read());
         }
     }
 }
